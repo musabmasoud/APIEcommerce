134cd73cf4651cb4eafa328ed480a4bbbafc732f
musab musab <musab.masoud@yahoo.com>
2024-03-14 23:54:58 +0300

    Add project files.

---
+++ APIEcommerce/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "List Of Products";
+        }
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "One Of Product";
+        }
+    }
+}
